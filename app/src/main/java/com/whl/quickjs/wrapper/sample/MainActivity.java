cf5f7b366aea3dbd59dc7daf41b8d2c9ea9f6a3c
sm-whl <81813780@qq.com>
2021-12-14 17:44:29 +0800

    refactor

--- app/src/main/java/com/whl/quickjs/wrapper/sample/MainActivity.java
+++ app/src/main/java/com/whl/quickjs/wrapper/sample/MainActivity.java
@@ -2,10 +2,7 @@ package com.whl.quickjs.wrapper.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "当时发生的", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, SecondActivity.class));
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
     }
 }
