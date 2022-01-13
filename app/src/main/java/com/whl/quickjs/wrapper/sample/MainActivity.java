963e963ec412dcd000cd61cc3c026e866696d1ce
HarlonWang <81813780@qq.com>
2022-01-13 15:27:56 +0800

    feat: simple show-how code

--- app/src/main/java/com/whl/quickjs/wrapper/sample/MainActivity.java
+++ app/src/main/java/com/whl/quickjs/wrapper/sample/MainActivity.java
@@ -3,12 +3,29 @@ package com.whl.quickjs.wrapper.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.whl.quickjs.wrapper.QuickJSContext;
 
 public class MainActivity extends AppCompatActivity {
+
+    QuickJSContext jsContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jsContext = QuickJSContext.create();
+        jsContext.evaluate("var text = 'Hello QuickJS';");
+        String text = jsContext.getGlobalObject().getStringProperty("text");
+        TextView textView = findViewById(R.id.text);
+        textView.setText(text);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        jsContext.destroyContext();
     }
 }
