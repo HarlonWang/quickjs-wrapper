51d86c867c4e5d32b6b3f2597b9a8a65744090e5
HarlonWang <81813780@qq.com>
2023-09-05 16:05:23 +0800

    临时: 使用 format 替换 __format_string

--- wrapper-android/src/main/java/com/whl/quickjs/android/QuickJSLoader.java
+++ wrapper-android/src/main/java/com/whl/quickjs/android/QuickJSLoader.java
@@ -82,6 +82,204 @@ public final class QuickJSLoader {
 
             return null;
         });
+        // todo 临时使用，后续改为模块导入方式
+        context.evaluate("const isRegExp = (v) => {\n" +
+                "    return Object.prototype.toString.call(v) === '[object RegExp]';\n" +
+                "}\n" +
+                "\n" +
+                "function format(value, opt) {\n" +
+                "    const defaultOpt = {\n" +
+                "        maxStringLength: 10000,\n" +
+                "        depth: 2,\n" +
+                "        maxArrayLength: 100,\n" +
+                "        seen: []\n" +
+                "    }\n" +
+                "    if (!opt) {\n" +
+                "        opt = defaultOpt\n" +
+                "    } else {\n" +
+                "        opt = Object.assign(defaultOpt, opt)\n" +
+                "    }\n" +
+                "\n" +
+                "    return formatValue(value, opt, 0)\n" +
+                "}\n" +
+                "\n" +
+                "function formatValue(value, opt, recurseTimes) {\n" +
+                "    if (typeof value !== 'object' && typeof value !== 'function') {\n" +
+                "        return formatPrimitive(value, opt)\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value === null) {\n" +
+                "        return 'null'\n" +
+                "    }\n" +
+                "\n" +
+                "    if (typeof value === 'function') {\n" +
+                "        return formatFunction(value)\n" +
+                "    }\n" +
+                "\n" +
+                "    if (typeof value === 'object') {\n" +
+                "        if (opt.seen.includes(value)) {\n" +
+                "            let index = 1\n" +
+                "            if (opt.circular === undefined) {\n" +
+                "                opt.circular = new Map()\n" +
+                "                opt.circular.set(value, index)\n" +
+                "            } else {\n" +
+                "                index = opt.circular.get(value)\n" +
+                "                if (index === undefined) {\n" +
+                "                    index = opt.circular.size + 1\n" +
+                "                    opt.circular.set(value, index)\n" +
+                "                }\n" +
+                "            }\n" +
+                "\n" +
+                "            return `[Circular *${index}]`\n" +
+                "        }\n" +
+                "\n" +
+                "        if (opt.depth !== null && ((recurseTimes - 1) === opt.depth)) {\n" +
+                "            if (value instanceof Array) {\n" +
+                "                return '[Array]'\n" +
+                "            }\n" +
+                "            return '[Object]'\n" +
+                "        }\n" +
+                "\n" +
+                "        recurseTimes++\n" +
+                "        opt.seen.push(value)\n" +
+                "        const string = formatObject(value, opt, recurseTimes)\n" +
+                "        opt.seen.pop()\n" +
+                "        return string\n" +
+                "    }\n" +
+                "}\n" +
+                "\n" +
+                "function formatObject(value, opt, recurseTimes) {\n" +
+                "    if (isRegExp(value)) {\n" +
+                "        return `${value.toString()}`\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value instanceof Promise) {\n" +
+                "        // quickjs 环境下通过 native 提供的方式获取 Promise 状态\n" +
+                "        if (typeof getPromiseState !== \"undefined\") {\n" +
+                "            const { result, state} = getPromiseState(value)\n" +
+                "            if (state === 'fulfilled') {\n" +
+                "                return `Promise { ${formatValue(result, opt, recurseTimes)} }`\n" +
+                "            } else if (state === 'rejected'){\n" +
+                "                return `Promise { <rejected> ${formatValue(result, opt, recurseTimes)} }`\n" +
+                "            } else if (state === 'pending'){\n" +
+                "                return `Promise { <pending> }`\n" +
+                "            }\n" +
+                "        } else {\n" +
+                "            return `Promise {${formatValue(value, opt, recurseTimes)}}`\n" +
+                "        }\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value instanceof Array) {\n" +
+                "        return formatArray(value, opt, recurseTimes)\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value instanceof Map) {\n" +
+                "        return formatMap(value, opt, recurseTimes)\n" +
+                "    }\n" +
+                "\n" +
+                "    let string = ''\n" +
+                "    for (const key in value) {\n" +
+                "        string += `${key}: `\n" +
+                "        string += formatValue(value[key], opt, recurseTimes)\n" +
+                "        string += ','\n" +
+                "        string += ' '\n" +
+                "    }\n" +
+                "\n" +
+                "    if (string.length > 0) {\n" +
+                "        // 这里需要删除下最后多余的逗号\n" +
+                "        string = '{ ' + string.substr(0, string.length - 2) + ' '\n" +
+                "    } else {\n" +
+                "        string = '{'\n" +
+                "    }\n" +
+                "\n" +
+                "    string += '}'\n" +
+                "    return string\n" +
+                "}\n" +
+                "\n" +
+                "function formatMap(value, opt, recurseTimes) {\n" +
+                "    let string = `Map(${value.size}) `\n" +
+                "    string += '{'\n" +
+                "    let isEmpty = true\n" +
+                "    value.forEach((v, k, map) => {\n" +
+                "        isEmpty = false\n" +
+                "        string += ` ${format(k, opt, recurseTimes)} => ${format(v, opt, recurseTimes)}`\n" +
+                "        string += ','\n" +
+                "    })\n" +
+                "\n" +
+                "    if (!isEmpty) {\n" +
+                "        // 删除最后多余的逗号\n" +
+                "        string = string.substr(0, string.length -1) + ' '\n" +
+                "    }\n" +
+                "\n" +
+                "    string += '}'\n" +
+                "    return string\n" +
+                "}\n" +
+                "\n" +
+                "function formatArray(value, opt, recurseTimes) {\n" +
+                "    let string = '['\n" +
+                "    value.forEach((item, index, array) => {\n" +
+                "        if (index === 0) {\n" +
+                "            string += ' '\n" +
+                "        }\n" +
+                "        string += formatValue(item, opt, recurseTimes)\n" +
+                "        if (index === opt.maxArrayLength - 1) {\n" +
+                "            string += `... ${array.length - opt.maxArrayLength} more item${array.length - opt.maxArrayLength > 1 ? 's' : ''}`\n" +
+                "        } else if (index !== array.length - 1) {\n" +
+                "            string += ','\n" +
+                "        }\n" +
+                "        string += ' '\n" +
+                "    })\n" +
+                "    string += ']'\n" +
+                "    return string\n" +
+                "}\n" +
+                "\n" +
+                "function formatFunction(value) {\n" +
+                "    let type = 'Function'\n" +
+                "\n" +
+                "    if (value.constructor.name === 'AsyncFunction') {\n" +
+                "        type = 'AsyncFunction'\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value.constructor.name === 'GeneratorFunction') {\n" +
+                "        type = 'GeneratorFunction'\n" +
+                "    }\n" +
+                "\n" +
+                "    if (value.constructor.name === 'AsyncGeneratorFunction') {\n" +
+                "        type = 'AsyncGeneratorFunction'\n" +
+                "    }\n" +
+                "\n" +
+                "    let fn = `${value.name ? `: ${value.name}` : ' (anonymous)'}`\n" +
+                "    return `[${type + fn}]`\n" +
+                "}\n" +
+                "\n" +
+                "function formatPrimitive(value, opt) {\n" +
+                "    const type = typeof value\n" +
+                "    switch (type) {\n" +
+                "        case \"string\":\n" +
+                "            return formatString(value, opt)\n" +
+                "        case \"number\":\n" +
+                "            return Object.is(value, -0) ? '-0' : `${value}`\n" +
+                "        case \"bigint\":\n" +
+                "            return `${String(value)}n`\n" +
+                "        case \"boolean\":\n" +
+                "            return `${value}`\n" +
+                "        case \"undefined\":\n" +
+                "            return \"undefined\"\n" +
+                "        default:\n" +
+                "            return value.toString\n" +
+                "    }\n" +
+                "}\n" +
+                "\n" +
+                "function formatString(value, opt) {\n" +
+                "    let trailer = ''\n" +
+                "    if (value.length > opt.maxStringLength) {\n" +
+                "        const remaining = value.length - opt.maxStringLength\n" +
+                "        value = value.splice(0, opt.maxStringLength)\n" +
+                "        trailer = `... ${remaining} more character${remaining > 1 ? 's' : ''}`\n" +
+                "    }\n" +
+                "\n" +
+                "    return `'${value + trailer}'`\n" +
+                "}", "format.js");
         context.evaluate("const console = {\n" +
                 "    log: (...args) => printLog(\"log\", ...args),\n" +
                 "    debug: (...args) => printLog(\"debug\", ...args),\n" +
@@ -94,14 +292,14 @@ public final class QuickJSLoader {
                 "    let arg = '';\n" +
                 "    if (args.length == 1) {\n" +
                 "        let m = args[0];\n" +
-                "        arg = __format_string(m);\n" +
+                "        arg = format(m);\n" +
                 "    } else if (args.length > 1) {\n" +
                 "        for (let i = 0; i < args.length; i++) {\n" +
                 "            if (i > 0) {\n" +
                 "                arg = arg.concat(', ');\n" +
                 "            }\n" +
                 "            let m = args[i];\n" +
-                "            arg = arg.concat(__format_string(m));\n" +
+                "            arg = arg.concat(format(m));\n" +
                 "        }\n" +
                 "    }\n" +
                 "\n" +
