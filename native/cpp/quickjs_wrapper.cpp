a7fcd2b0053d33af26a35caa29c51088e67dd774
HarlonWang <81813780@qq.com>
2024-03-21 13:45:28 +0800

    优化：使用 ByteArray 解决 jvm 平台上 unicode 转换问题

--- native/cpp/quickjs_wrapper.cpp
+++ native/cpp/quickjs_wrapper.cpp
@@ -378,9 +378,19 @@ jobject QuickJSWrapper::toJavaObject(JNIEnv *env, jobject thiz, JSValueConst& th
         }
 
         case JS_TAG_STRING: {
-            const char* string = JS_ToCString(context, value);
-            result = env->NewStringUTF(string);
-            JS_FreeCString(context, string);
+            const char *str;
+            size_t len;
+            str = JS_ToCStringLen(context, &len, value);
+
+            jbyteArray jba = env->NewByteArray(len);
+            env->SetByteArrayRegion(jba, 0, len, reinterpret_cast<const jbyte *>(str));
+
+            result = env->NewObject(stringClass,
+                                    env->GetMethodID(stringClass, "<init>",
+                                                     "([B)V"), jba);
+
+            JS_FreeCString(context, str);
+            env->DeleteLocalRef(jba);
             // JSString 类型的 JSValue 需要手动释放掉，不然会泄漏
             JS_FreeValue(context, value);
             break;
@@ -554,11 +564,19 @@ jstring QuickJSWrapper::jsonStringify(JNIEnv *env, jlong value) const {
         return nullptr;
     }
 
-    auto result = JS_ToCString(context, obj);
+    const char *str;
+    size_t len;
+
+    str = JS_ToCStringLen(context, &len, obj);
+    jbyteArray jba = env->NewByteArray(len);
+    env->SetByteArrayRegion(jba, 0, len, reinterpret_cast<const jbyte *>(str));
+    jstring ret = static_cast<jstring>(env->NewObject(stringClass,
+                                                      env->GetMethodID(stringClass, "<init>",
+                                                                       "([B)V"), jba));
     JS_FreeValue(context, obj);
-    jstring string =env->NewStringUTF(result);
-    JS_FreeCString(context, result);
-    return string;
+    JS_FreeCString(context, str);
+    env->DeleteLocalRef(jba);
+    return ret;
 }
 
 jint QuickJSWrapper::length(JNIEnv *env, jlong value) const {
