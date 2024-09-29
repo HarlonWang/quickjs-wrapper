71778e9b8578b5cde7680b417467b2b889a455ef
HarlonWang <81813780@qq.com>
2024-09-29 10:57:52 +0800

    feat: 增加 MapFilter 注释

--- wrapper-java/src/main/java/com/whl/quickjs/wrapper/MapFilter.java
+++ wrapper-java/src/main/java/com/whl/quickjs/wrapper/MapFilter.java
@@ -5,6 +5,13 @@ package com.whl.quickjs.wrapper;
  */
 public interface MapFilter {
 
+    /**
+     * toMap 转换期间是否需要过滤指定 key
+     * @param key 需要过滤的 key
+     * @param pointer 当前 key 所属对象的指针地址
+     * @param extra 扩展参数，如果你需要一些额外信息，可通过 toMap(..., extra) 传递该参数
+     * @return 是否需要过滤该 key
+     */
     boolean shouldSkipKey(String key, long pointer, Object extra);
 
 }
